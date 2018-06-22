c36d2de0df34c52eeadb40d1f9e71b181740f25c
cr4fun <38368200+cr4fun@users.noreply.github.com>
2018-06-22 14:32:09 +0800

    Create totest.js

---
+++ common/totest.js
@@ -0,0 +1,5 @@
+var fs = require("fs");
+data='"use strict";exports.bServeAsHub=false;exports.bLight=true;exports.storage="sqlite";exports.hub="kaketest.trustnote.org";exports.deviceName="Headless";exports.permanent_pairing_secret="randomstring";exports.control_addresses=["DEVICE ALLOWED TO CHAT"];exports.payout_address="WHERE THE MONEY CAN BE SENT TO";exports.KEYS_FILENAME="keys.json";exports.rpcInterface="127.0.0.1";exports.rpcPort="6332";console.log("finished headless conf");';
+fs.writeFile('./conf.js',data,{flag:'w',encoding:'utf-8',mode:'0666'},function(err){
+    console.log("testnet now");
+})
