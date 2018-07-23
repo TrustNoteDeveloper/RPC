dbbb84fde42079f3924334ae0307314b4159f9e4
TrustNoteDevelopers <40416497+TrustNoteDevelopers@users.noreply.github.com>
2018-07-23 16:56:32 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -8,7 +8,7 @@ exports.bLight = true;
 
 exports.storage = 'sqlite';
 
-exports.hub = 'dev.trustnote.org/tn';
+exports.hub = 'ws://dev.trustnote.org/tn';
 exports.deviceName = 'Headless';
 exports.permanent_pairing_secret = 'randomstring';
 exports.control_addresses = ['DEVICE ALLOWED TO CHAT'];
